cff8a694ec5ce01623362f7fc1850c5c99bd7f5b
tula83 <131385747+tula83@users.noreply.github.com>
2023-09-14 08:49:12 +0530

    Create List.py

---
+++ Python/List.py
@@ -0,0 +1,5 @@
+movies=['titanic','3 ididots','munna bhan mbbs','irishman','lords of the rings','catch me if you can']
+print(movies[3])
+movies.append('yeh hae dil muskil')
+new_list=movies[:3]
+print(new_list)
