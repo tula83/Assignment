9f5e2620bbe7817a8abbe9582fab4ddc16f29b86
tula83 <131385747+tula83@users.noreply.github.com>
2023-09-22 09:18:19 +0530

    Create App.js

---
+++ ReactJs/Whiteboard/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './components/Login';
+import  WhiteBoard from './components/WhiteBoard';
+import Display from './components/Display';
+function App() {
+  return (
+    <div className="App">
+    
+        <WhiteBoard/>
+    </div>
+  );
+}
+
+export default App;
