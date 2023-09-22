8d5283d40a2924b9a296271d551aa30a46bb5abb
tula83 <131385747+tula83@users.noreply.github.com>
2023-09-22 09:16:50 +0530

    Create Fetchapi.js
    This includes implementation of  fetch api with async function

---
+++ ReactJs/Fetchapi.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+
+const Display = () => {
+
+ const [data,setData]=useState([]);
+ const [isloading,setIsloading]=useState(true)
+
+async   function  getData(){
+
+  try{
+
+    const response=await fetch(`https://dummyjson.com/products`)
+    
+    if(!response.ok){
+        throw new Error(`HTTP  error ${response.status}`)
+    }
+    const data=response.json();
+    setData(data);
+    setIsloading(false);
+
+
+  }
+  catch(error){
+     console.log('error is',error)
+  }
+
+
+    
+   }
+
+    useEffect(()=>{
+         getData();
+    },[])
+
+  return (
+    <ul>
+      {
+        !isloading?
+        (data.map((item)=>{
+            return(
+
+            <div key={item.id}>
+              <li>{item.id}</li>
+              <li>{item.title}</li>
+            </div>    
+            );
+        })
+        ):
+         <h2>data is loading......</h2>
+      }
+    
+    </ul>
+  )
+}
+
+export default Display;
